07bf4708afacadb2dcf0cf18659105925e180a2b
Your Name <you@example.com>
2023-12-10 19:42:04 +0200

    shared

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,42 @@
-const headerBurger = document.querySelector('.header-burger');
-const burgerBackdrop = document.querySelector('.burger-backdrop')
+// const headerBurger = document.querySelector('.header-burger');
+// const burgerBackdrop = document.querySelector('.burger-backdrop');
+// const headerBurgerMenu = document.querySelector('.header-menu');
 
-if (headerBurger) {
-  const headerBurgerMenu = document.querySelector('.header-menu');
-  headerBurger.addEventListener("click", function (e) {
+// if (headerBurger) {
+//   headerBurger.addEventListener("click", function (e) {
+//     if(document.body.classList.contains('lock')){
+//       document.body.style.width = "auto";
+//       headerBurger.classList.toggle('header-burger_shift')
+//     } else {
+//       document.body.style.width = document.body.clientWidth + 'px';
+//       headerBurger.classList.toggle('header-burger_shift')
+//     }
+//     document.body.classList.toggle('lock');
+//     // burgerBackdrop.classList.toggle('active')
+//     headerBurger.classList.toggle('active');
+//     headerBurgerMenu.classList.toggle('active');
+//   });
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('.header-burger')
+  .addEventListener("click", function (e) {
+    const headerBurgerMenu = document.querySelector('.header-menu');
+    const headerBurger = document.querySelector('.header-burger');
+    const burgerBackdrop = document.querySelector('.burger-backdrop');
+      
     if(document.body.classList.contains('lock')){
       document.body.style.width = "auto";
-      headerBurger.classList.toggle('.header-burger_shift')
+      headerBurger.classList.toggle('header-burger_shift')
     } else {
       document.body.style.width = document.body.clientWidth + 'px';
-      headerBurger.classList.toggle('.header-burger_shift')
+      headerBurger.classList.toggle('header-burger_shift')
     }
     document.body.classList.toggle('lock');
     burgerBackdrop.classList.toggle('active')
     headerBurger.classList.toggle('active');
     headerBurgerMenu.classList.toggle('active');
   });
-}
+});
+
+
