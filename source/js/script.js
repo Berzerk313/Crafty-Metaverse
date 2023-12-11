ff5dbb3a60b7c8d9f802dc895c647cf1d3493528
Your Name <you@example.com>
2023-12-11 17:43:56 +0200

    shared

--- source/js/script.js
+++ source/js/script.js
@@ -20,23 +20,37 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('.header-burger')
-  .addEventListener("click", function (e) {
-    const headerBurgerMenu = document.querySelector('.header-menu');
-    const headerBurger = document.querySelector('.header-burger');
-    const burgerBackdrop = document.querySelector('.burger-backdrop');
-      
-    if(document.body.classList.contains('lock')){
-      document.body.style.width = "auto";
-      headerBurger.classList.toggle('header-burger_shift')
-    } else {
-      document.body.style.width = document.body.clientWidth + 'px';
-      headerBurger.classList.toggle('header-burger_shift')
-    }
-    document.body.classList.toggle('lock');
-    burgerBackdrop.classList.toggle('active')
-    headerBurger.classList.toggle('active');
-    headerBurgerMenu.classList.toggle('active');
-  });
+    .addEventListener("click", function (e) {
+      const headerBurgerMenu = document.querySelector('.header-menu');
+      const headerBurger = document.querySelector('.header-burger');
+      const burgerBackdrop = document.querySelector('.burger-backdrop');
+
+      if (document.body.classList.contains('lock')) {
+        document.body.style.width = "auto";
+        headerBurger.classList.toggle('header-burger_shift')
+      } else {
+        document.body.style.width = document.body.clientWidth + 'px';
+        headerBurger.classList.toggle('header-burger_shift')
+      }
+      document.body.classList.toggle('lock');
+      burgerBackdrop.classList.toggle('active')
+      headerBurger.classList.toggle('active');
+      headerBurgerMenu.classList.toggle('active');
+    });
 });
 
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  let scrollPos = 10;
+  let header = document.querySelector('.header')
+
+  if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
+    header.classList.add('header_background');
+  }
+  else {
+    header.classList.remove('header_background');
+  }
+}
 
